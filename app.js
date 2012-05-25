a4c6937c042e5a530dcaa72428354aae0a528df4
luiztiago <luiztiago@gmail.com>
2012-05-25 12:23:59 -0300

    working with get and post methods too

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var app = require('express').createServer(),
 	jade = require('jade'),
+	url = require('url'),
+	qs = require('querystring'),
 	cep = require('./cep.js');
 
 app.configure(function(){
@@ -17,6 +19,33 @@ app.get('/', function(req, res){
 app.get('/cep.:format?/:cep', function(req, res){
 	var string = req.params.cep,
 		format = req.params.format;
+	cep.search(res, string, format);
+});
+
+app.get('/cep/', function(req, res){
+
+	var string,
+		format;
+
+	if(req.method == "POST") {
+		var body = '';
+		req.on('data', function (data) {
+			body += data;
+		});
+
+		string = (body.cep) ? body.cep : '';
+		format = (body.format) ? body.format : 'json';
+
+	}else if(req.method == 'GET') {
+		var url_parts = url.parse(req.url, true);
+
+		string = (url_parts.query.cep) ? url_parts.query.cep : '';
+		format = (url_parts.query.format) ? url_parts.query.format : 'json';
+		
+	}else{
+		return false;
+	}
+
 	cep.search(res, string, format);
 });
 
